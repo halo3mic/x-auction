49239fc41e671721b0cc13cf1acc32799052d09f
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-08-01 23:35:59 +0100

    decalare wished tests

---
+++ test/TokenAuction.t.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.9;
+
+import "../lib/suave-std/src/Test.sol";
+import "forge-std/console2.sol";
+
+import "../src/Auction.sol";
+
+
+contract AuctionTest is Test, SuaveEnabled {
+
+    function test_createAuction() public {}
+    function test_cancelAuction() public {}
+    function test_bid() public {}
+
+    function test_checkBidValidity_auctionStatus() public {}
+    function test_checkBidValidity_auctionDeadline() public {}
+    function test_checkBidValidity_nonSufficientFunds() public {}
+    function test_checkBidValidity_zeroBid() public {}
+    function test_checkBidValidity_sufficientFunds() public {}
+
+    function test_settleVickeryAuction_singleBid() public {}
+    function test_settleVickeryAuction_twoEqualBids() public {}
+    function test_settleVickeryAuction_twoEqualOneUniqueBid() public {}
+    function test_settleVickeryAuction_twoUniqueBids() public {}
+    function test_settleVickeryAuction_threeUniqueBids() public {}
+
+}
---
+++ test/Vault.t.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.9;
+
+import "forge-std/Test.sol";
+
+
+contract VaultTest is Test {
+
+    function test_getBalance() public {}
+    function test_lock() public {}
+    function test_scheduleUnlock() public {}
+    function test_withdraw() public {}
+    function test_takeFunds() public {}
+
+}
