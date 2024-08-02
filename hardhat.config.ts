b9448f7b8fc5cbe149f62c9d28020e2f2ff8ec58
halo3mic <46010359+halo3mic@users.noreply.github.com>
2024-08-02 01:52:36 +0100

    add hh

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+import "hardhat-deploy";
+import {config as dconfig} from "dotenv";
+
+
+dconfig();
+const HOLESKY_RPC = getEnvValSafe('HOLESKY_RPC')
+const HOLESKY_PK = getEnvValSafe('HOLESKY_PK')
+const TOLIMAN_RPC = getEnvValSafe('TOLIMAN_RPC')
+const TOLIMAN_PK = getEnvValSafe('TOLIMAN_PK')
+
+const config = {
+	solidity: '0.8.13',
+	defaultNetwork: 'toliman',
+	namedAccounts: {
+		deployer: {
+			default: 0,
+		}
+	},
+	networks: {
+		holesky: {
+			chainId: 17000,
+			url: HOLESKY_RPC,
+			accounts: [ HOLESKY_PK ],
+		}, 
+		toliman: {
+			chainId: 33626250, 
+			url: TOLIMAN_RPC,
+			accounts: [ TOLIMAN_PK ], 
+			companionNetworks: {
+				holesky: 'holesky',
+			},
+		}
+	}
+};
+
+export default config;
+
+
+function getEnvValSafe(key: string): string {
+	const endpoint = process.env[key]
+	if (!endpoint)
+		throw(`Missing env var ${key}`)
+	return endpoint
+}
